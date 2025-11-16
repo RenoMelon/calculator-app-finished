28640c6146e1fe17e08ef25ddf82d87e28fbd98b
Reno Melon <92853707+RenoMelon@users.noreply.github.com>
2025-11-16 12:12:06 +0100

    Update server to listen on all network interfaces

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require("./app");
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
-app.listen(port);
-console.log("Server running on port: " + port);
+app.listen(port, host, () => {
+  console.log(`Server draait op http://${host}:${port}`);
+});
